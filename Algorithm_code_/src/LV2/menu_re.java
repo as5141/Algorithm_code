b653b8e4c068de9267df1f0d2ae4998b99c7f701
minjung <as5141@naver.com>
2022-01-28 00:09:36 +0900

    20220128_1

--- Algorithm_code_/src/LV2/menu_re.java
+++ Algorithm_code_/src/LV2/menu_re.java
@@ -1,8 +1,6 @@
 package LV2;
 
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class menu_re {
 	static HashMap<String,Integer> map;
@@ -14,11 +12,17 @@ public class menu_re {
 	}
 
 	public static String[] solution(String[] orders, int[] course) {
+		//Queue 선언 (우선순위가 낮은 숫자 순)
 	       PriorityQueue<String> pq = new PriorityQueue<>();
+	       //course 반복
 	        for (int i=0;i<course.length;i++){
+	        	//생성해둔 map 선언 및 초기화
 	            map = new HashMap<>();
+	            //전역변수 초기화
 	            m=0;
+	            //
 	            for (int j=0;j<orders.length;j++) {
+	            	//find 함수 실행 cnt, str, targetNum, idx, word
 	                find(0, "", course[i], 0, orders[j]);
 	            }
 	            for (String s : map.keySet()){
@@ -35,16 +39,24 @@ public class menu_re {
 	        return ans;
 	    }
 	    static void find(int cnt,String str,int targetNum,int idx,String word){
+	    	//cnt가 targetNum과 같다면
 	        if (cnt==targetNum){
+	        	//str을 char 배열로 배열화
 	            char[] c = str.toCharArray();
+	            //정렬
 	            Arrays.sort(c);
+	            //temps 초기화
 	            String temps="";
+	            //문자열 길이 만큼 반복
 	            for (int i=0;i<c.length;i++)temps+=c[i];
+	            //map.put(2, 
 	            map.put(temps,map.getOrDefault(temps,0)+1);
+	            //
 	            m = Math.max(m,map.get(temps));
 	            return;
 	        }
 	        for (int i=idx;i<word.length();i++){
+	        	//now = orders.charAt(i)
 	            char now =word.charAt(i);
 	            find(cnt+1,str+now,targetNum,i+1,word);
 	        }
